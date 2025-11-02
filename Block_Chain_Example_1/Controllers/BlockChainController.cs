18a136f1ba15a817e9b9f18c245f37dbe59c24ac
SerhiiRudenok <svaru@ukr.net>
2025-11-03 01:51:59 +0200

    Добавьте файлы проекта.

---
+++ Block_Chain_Example_1/Controllers/BlockChainController.cs
@@ -0,0 +1,218 @@
+﻿using Block_Chain_Example_1.Hubs;
+using Block_Chain_Example_1.Models;
+using Block_Chain_Example_1.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging.Signing;
+using System.Globalization;
+using System.Net;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace Block_Chain_Example_1.Controllers
+{
+    public class BlockChainController : Controller
+    {
+        private static CancellationTokenSource _miningCts;
+
+        private static BlockChainService blockChainService = new BlockChainService();
+
+        // Index
+        public IActionResult Index()
+        {
+            ViewBag.Valid = blockChainService.IsValid();                           // Перевірити валідність всього блокчейну
+            ViewBag.FirstInvalidIndex = blockChainService.GetFirstInvalidIndex();  // Отримати індекс першого невалідного блоку
+            ViewBag.Difficulty = blockChainService.Difficulty;                     // Поточна складність майнінгу
+            ViewBag.PrivateKey = blockChainService.PrivateKeyXml;                  // Приватний ключ майнера у форматі XML
+            ViewBag.PublicKey = blockChainService.PublicKeyXml;                    // Публічний ключ майнера у форматі XML
+            ViewBag.MempoolCount = blockChainService.Mempool.Count;                 // Кількість транзакцій у мемпулі
+            ViewBag.Mempool = blockChainService.Mempool;                            // Список транзакцій у мемпулі
+            ViewBag.Wallets = blockChainService.Wallets.Values.ToList();            // Список зареєстрованих гаманців
+            ViewBag.HasMinedBlock = TempData["HasMinedBlock"] as bool? ?? false;     // Чи є збережені результати майнінгу для додавання блоку
+            return View(blockChainService.Chain);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult RegisterWallet(string PublicKeyXml, string displayName)    // Реєстрація нового гаманця
+        {
+            var wallet = blockChainService.RegisterWallet(PublicKeyXml, displayName);
+            return RedirectToAction("Index");
+        }
+
+
+        // Створення нової транзакції
+        [HttpPost]
+        public IActionResult CreateTransaction(string fromAddress, string toAddress, decimal amount, decimal fee, string privateKey, string note)
+        {
+            var tx = new Models.Transaction
+            {
+                FromAddress = fromAddress,
+                ToAddress = toAddress,
+                Amount = amount,
+                Fee = fee,
+                Note = note
+            };
+            tx.Signature = BlockChainService.SignPayload(tx.CanonicalPayload(), privateKey);
+            try
+            {
+                blockChainService.CreateTransaction(tx);
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        //Функція майнінгу
+        [HttpPost]
+        public IActionResult MinePending(string privateKey)     // Майнінг нового блоку з транзакціями з мемпулу
+        {
+            try
+            {
+                blockChainService.MinePending(privateKey);
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult DemoSetup()
+        {
+            var (Ivan, prvKey1) = blockChainService.CreateWallet("Ivan");
+            var (Taras, prvKey2) = blockChainService.CreateWallet("Taras");
+
+            decimal amount = 10.0m;
+            decimal fee = 0.5m;
+
+            var tx = new Models.Transaction
+            {
+                FromAddress = Ivan.Address,
+                ToAddress = Taras.Address,
+                Amount = amount,
+                Fee = fee,
+                Note = "Payment for services"
+            };
+
+            var sig = BlockChainService.SignPayload(tx.CanonicalPayload(), prvKey1);
+
+            tx.Signature = sig;
+
+            blockChainService.CreateTransaction(tx);
+
+            return RedirectToAction("Index");
+        }
+
+
+        // Зупинка майнінгу блоку
+        [HttpPost]
+        public IActionResult CancelMining()
+        {
+            _miningCts?.Cancel(); // встановлює ct.IsCancellationRequested = true
+            TempData["HasMinedBlock"] = false;
+            TempData.Clear();
+            return RedirectToAction("Index");
+        }
+
+
+        // GET: BlockChainController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BlockChainController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BlockChainController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var block = blockChainService.Chain.FirstOrDefault(b => b.Index == id);
+            if (block == null)
+                return NotFound();
+
+            return View(block);
+        }
+
+        // POST: BlockChainController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Block_Chain_Example_1.Models.Block updatedBlock)
+        {
+            var block = blockChainService.Chain.FirstOrDefault(b => b.Index == id);
+            if (block == null)
+                return NotFound();
+
+            block.Signature = updatedBlock.Signature; // оновити підпис
+            block.Hash = updatedBlock.Hash;           // оновити хеш
+
+            ViewBag.Valid = blockChainService.IsValid();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Пошук блоку за індексом або хешем
+        [HttpGet]
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(string query)
+        {
+            var block = blockChainService.FindBlock(query);
+            ViewBag.FoundBlock = block;
+            ViewBag.IsPost = true;
+            return View();
+        }
+
+
+        // Генерація нового приватного ключа RSA-ключа
+        [HttpPost]
+        public IActionResult GenerateKey()
+        {
+            // Генерація RSA-ключа
+            using var rsa = RSA.Create(512);
+            byte[] privateKeyBytes = rsa.ExportRSAPrivateKey();
+            string base64Key = Convert.ToBase64String(privateKeyBytes);
+
+            ViewBag.GeneratedKey = base64Key;
+            ViewBag.Difficulty = blockChainService.Difficulty; // коли генерується ключ потрібно зберегти складність майнінгу
+
+            // Повернення на Index.cshtml
+            return View("Index", blockChainService.Chain);
+        }
+
+        [HttpPost]
+        public IActionResult SetDifficulty(int difficulty) { 
+            if(difficulty < 1) difficulty = 1;
+            if(difficulty > 10) difficulty = 10;
+            blockChainService.Difficulty = difficulty;
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Block_Chain_Example_1/Services/BlockChainService.cs
@@ -0,0 +1,153 @@
+﻿using Block_Chain_Example_1.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Block_Chain_Example_1.Services
+{
+    public class BlockChainService
+    {
+        public List<Block> Chain { get; set; } = new List<Block>();
+
+        public int Difficulty { get; set; } = 3; // Визначає складність майнінгу (кількість провідних нулів у хеші)
+        public string PrivateKeyXml { get; set; } // Зовнішній доступ до приватного ключа у форматі XML для підпису блоків поза класом
+        public string PublicKeyXml { get; set; } // Зовнішній доступ до публічного ключа у форматі XML для перевірки підписів поза класом
+
+        public Dictionary<string, Wallet> Wallets { get; set; } = new Dictionary<string, Wallet>(); // Словник гаманців за адресою
+        public List<Transaction> Mempool { get; set; } = new List<Transaction>(); // Список непідтверджених транзакцій
+        public const decimal MinerReward = 1.0m; // Фіксована винагорода майнеру за кожен знайдений блок
+
+
+        public BlockChainService()
+        {
+            var rsa = RSA.Create();                     //  створюю нову пару ключів: приватний і публічний
+            PrivateKeyXml = rsa.ToXmlString(true);      // Зберігаю приватний ключ для підпису блоків
+            PublicKeyXml = rsa.ToXmlString(false);      // Зберігаю публічний ключ у форматі XML для зберігання в блоці
+
+            var block = new Block(0, "");               // Створюю генезіс-блок (перший блок у ланцюжку) з індексом 0 та порожнім PrevHash
+            block.Sign(PrivateKeyXml, PublicKeyXml);        // Підписую генезіс-блок за допомогою приватного ключа
+            Chain.Add(block);                               // Додаю генезіс-блок до ланцюжка
+        }
+
+        public Wallet RegisterWallet(string publicKeyXml, string displayName) // Реєстрація нового гаманця
+        {
+            var wallet = new Wallet
+            {
+                PublicKeyXml = publicKeyXml,
+                Address = Wallet.DereveAddressFromPublicKeyXml(publicKeyXml),
+                DisplayName = displayName,
+            };
+            Wallets[wallet.Address] = wallet;
+            return wallet;
+        }
+
+        public void CreateTransaction(Transaction transaction)      // Створення нової транзакції
+        {
+            var rsa = RSA.Create();
+            var wallet = Wallets[transaction.FromAddress];
+            rsa.FromXmlString(wallet.PublicKeyXml);
+            var payload = Encoding.UTF8.GetBytes(transaction.CanonicalPayload());
+            var sig = Convert.FromBase64String(transaction.Signature);
+            if (!rsa.VerifyData(payload, sig, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1))
+                throw new Exception("Недійсний підпис транзакції"); // Якщо підпис недійсний, викидаю виключення
+            Mempool.Add(transaction);                                      // Додаю транзакцію до мемпулу
+        }
+
+        public Block MinePending(string privateKey)         // Майнінг нового блоку з транзакціями з мемпулу
+        {
+            var rsa = RSA.Create();
+            rsa.FromXmlString(privateKey);
+            var publicMinerKey = rsa.ToXmlString(false);
+            var minerAdress = Wallets.Values.FirstOrDefault(w => w.PublicKeyXml == publicMinerKey)?.Address;
+
+            decimal totalFee = Mempool.Sum(t => t.Fee); // Обчислюю загальну суму комісій у мемпулі
+            var baht = new List<Transaction>() {
+                new Transaction()
+                {
+                    FromAddress = "COINBASE",
+                        ToAddress = minerAdress,
+                        Amount = MinerReward + totalFee,
+                }
+            };
+            baht.AddRange(Mempool); // Додаю всі транзакції з мемпулу до блоку
+            var PrevBlock = Chain[Chain.Count - 1]; // Отримую останній блок у ланцюжку
+            var newBlock = new Block(Chain.Count, PrevBlock.Hash); // Створюю новий блок: індекс = порядковий номер, дані = порожні, PrevHash = хеш останнього блоку
+            newBlock.SetTransaction(baht);               // Встановлюю транзакції у новий блок
+            newBlock.Mine(Difficulty);                  // Генерую новий блок з заданою складністю
+
+            newBlock.Sign(privateKey, publicMinerKey); // Підписую новий блок за допомогою приватного ключа, зберігаю публічний ключ у блоці для подальшої перевірки
+            Chain.Add(newBlock);                        // Додаю новий блок до ланцюжка
+
+            Mempool.Clear();                            // Очищую мемпул після додавання транзакцій до блоку
+
+            return newBlock;
+        }
+
+
+        public bool IsValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)                        // Починаю перевірку з другого блоку (індекс 1)
+            {
+                var current = Chain[i];                                  // Поточний блок
+                var prev = Chain[i - 1];                            // Попередній блок
+
+                if (current.PrevHash != prev.Hash) return false;    // Перевірка відповідності хешів
+                if (current.Hash != current.ComputeHash()) return false; // Перевірка валідності хешу
+                if (!current.Verify()) return false;                     // Перевірка валідності підпису
+                if(!current.HashValidProof()) return false;               // Перевірка відповідності хешу вимогам складності
+            }
+            return true;
+        }
+
+        public (Wallet wallet, string privateKeyXml) CreateWallet(string displayName)   // Створення нового гаманця з парою ключів
+        {
+            var rsa = RSA.Create();                             // Створюю нову пару ключів для гаманця
+            var privateKeyXml = rsa.ToXmlString(true);         // Експортую приватний ключ у форматі XML
+            var publicKeyXml = rsa.ToXmlString(false);         // Експортую публічний ключ у форматі XML
+            var wallet = RegisterWallet(publicKeyXml, displayName); // Реєструю гаманець з публічним ключем і відображуваним ім'ям
+            return (wallet, privateKeyXml); // Повертаю гаманець і приватний ключ   
+        }
+
+
+        public static string SignPayload(string payload, string privateKeyXml)      // Підписати довільний текстовий рядок за допомогою приватного ключа у форматі XML
+        {
+            var rsa = RSA.Create();                         // Створюю новий екземпляр RSA для підпису
+            rsa.FromXmlString(privateKeyXml);               // Імпортую приватний ключ з XML
+            var data = Encoding.UTF8.GetBytes(payload);     // Конвертую текстовий рядок у байти
+            var sig = rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);  // Підписую дані з використанням SHA256
+            return Convert.ToBase64String(sig);             // Повертаю підпис у форматі Base64
+        }
+
+
+
+        public int? GetFirstInvalidIndex()      // Пошук індексу першого невалідного блоку
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                var current = Chain[i];
+                var prevBlock = Chain[i - 1];
+
+                bool hashIsMatch = current.PrevHash == prevBlock.Hash;    // Перевірка відповідності хешів
+                bool hashIsValid = current.Hash == current.ComputeHash(); // Перевірка валідності хешу
+                bool signatureIsValid = current.Verify();                 // Перевірка валідності підпису
+
+                if (!hashIsMatch || !hashIsValid || !signatureIsValid)
+                    return i;
+            }
+
+            return null;
+        }
+
+        public Block FindBlock(string query)  // Пошук блоку за хешем або індексом
+        {
+            if (int.TryParse(query, out int index))
+            {
+                return Chain.FirstOrDefault(b => b.Index == index);
+            }
+            else
+            {
+                return Chain.FirstOrDefault(b => b.Hash.Equals(query, StringComparison.OrdinalIgnoreCase));
+            }
+        }
+
+    }
+}
